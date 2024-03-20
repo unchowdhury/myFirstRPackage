8e4135e0eef5496f61413400395c11f3e3aa2b2a
unchowdhury <43548882+unchowdhury@users.noreply.github.com>
2024-03-20 14:48:55 +1000

    Update dinoPlot.R to plot stars

--- R/dinoPlot.R
+++ R/dinoPlot.R
@@ -11,6 +11,6 @@
 #' @export
 dinoPlot <- function(col = "black", add = T){
   dat=datasauRus::datasaurus_dozen # Get the data from the package
-  dat=dat[which(dat$dataset=="dino"),] # Subset the data
+  dat=dat[which(dat$dataset=="star"),] # Subset the data
   plot(dat$x, dat$y, col=col, pch=20, cex=2)
 }
